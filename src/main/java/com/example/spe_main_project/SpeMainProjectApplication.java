b557d7909793a1ca96aacbe8d0991bb390b554f5
Naidu2658 <kodanda.brahma@iiitb.ac.in>
2022-04-20 19:12:49 +0530

    created project

---
+++ src/main/java/com/example/spe_main_project/SpeMainProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.spe_main_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeMainProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeMainProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spe_main_project/SpeMainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spe_main_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeMainProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
